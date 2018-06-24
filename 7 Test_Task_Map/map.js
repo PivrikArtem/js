324e798f26c2ac00024b1491af0ff0fb626e82ce
Pivrik Artem <artempivrik90@gmail.com>
2018-06-24 23:58:16 +0200

    add new file app.js

---
+++ 7 Test_Task_Map/map.js
@@ -0,0 +1,38 @@
+var map = new Map([
+    {
+        city: 'Nashville, TN',
+        lat: 36.17,
+        long: -86.78
+    },
+    {
+        city: 'New York, NY',
+        lat: 40.71,
+        long: -74.00
+    },
+    {
+        city: 'Atlanta, GA',
+        lat: 33.75,
+        long: -84.39
+    },
+    {
+        city: 'Denver, CO',
+        lat: 39.74,
+        long: -104.98
+    },
+    {
+        city: 'Seattle, WA',
+        lat: 47.61,
+        long: -122.33
+    },
+    {
+        city: 'Los Angeles, CA',
+        lat: 34.05,
+        long: -118.24
+    },
+
+    {
+        city: 'Memphis, TN',
+        lat: 35.15,
+        long: -90.05
+    },
+]);
