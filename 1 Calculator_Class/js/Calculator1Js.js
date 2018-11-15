eddfe182d303fe0e131be133e1c6d4037f55d58c
Artem <37675071+PivrikArtem@users.noreply.github.com>
2018-11-15 22:01:27 +0200

    Delete Calculator1Js.js

--- 1 Calculator_Class/js/Calculator1Js.js
+++
@@ -1,9 +0,0 @@
-var calculator1 = new calculator1Js('js-calculator1');
-var calculator2 = new calculator1Js('js-calculator2');
-function anotherOneCalculator(calculator) {
-    calculator.render();
-    calculator.initialization();
-    calculator.addListener();
-}
-anotherOneCalculator(calculator1);
-anotherOneCalculator(calculator2);
